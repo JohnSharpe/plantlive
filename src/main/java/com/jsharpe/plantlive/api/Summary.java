9b3fbce50ad7160b9fd8764ba8d1b57b0fb5bdb3
JohnSharpe <john.sharpe@live.com>
2019-05-22 13:37:40 +0100

    Added a serializable API object and a couple of tests.

---
+++ src/main/java/com/jsharpe/plantlive/api/Summary.java
@@ -0,0 +1,84 @@
+package com.jsharpe.plantlive.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * This class is a summary of a given plant right now.
+ * The 4 numbers (thlc) are averages over the past x hours (configurable).
+ * This might be serialized to JSON or translated to an HTML UI.
+ */
+public class Summary {
+
+    // TODO this might eventually be a uuid (i.e. obscuring database state)
+    private final long id;
+
+    // TODO this might eventually be an enum
+    private final String type;
+
+    private final int temperature;
+    private final int humidity;
+    private final int light;
+    private final int conductivity;
+
+    // TODO include ranges/parameters for bad, medium, good, perfect etc.
+
+    /**
+     * These annotations enable Dropwizard-blessed serialization testing, but we don't accept Summaries externally.
+     * Constraints mentioned below are to be maintained by the database.
+     *
+     * @param id           the plant's unique id
+     * @param type         the plant's not-null type. Will later help us to determine how a plant 'should' be.
+     * @param temperature  the average temperature of the air around the plant
+     * @param humidity     the average humidity of the air around the plant
+     * @param light        the average light level the plant is exposed to
+     * @param conductivity the average conductivity (wetness) of the soil
+     */
+    @JsonCreator
+    public Summary(
+            @JsonProperty("id") long id,
+            @JsonProperty("type") String type,
+            @JsonProperty("temperature") int temperature,
+            @JsonProperty("humidity") int humidity,
+            @JsonProperty("light") int light,
+            @JsonProperty("conductivity") int conductivity
+    ) {
+        this.id = id;
+        this.type = type;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.light = light;
+        this.conductivity = conductivity;
+    }
+
+    @JsonProperty("id")
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("temperature")
+    public int getTemperature() {
+        return temperature;
+    }
+
+    @JsonProperty("humidity")
+    public int getHumidity() {
+        return humidity;
+    }
+
+    @JsonProperty("light")
+    public int getLight() {
+        return light;
+    }
+
+    @JsonProperty("conductivity")
+    public int getConductivity() {
+        return conductivity;
+    }
+
+}
---
+++ src/test/java/com/jsharpe/plantlive/api/SummarySerializationTest.java
@@ -0,0 +1,48 @@
+package com.jsharpe.plantlive.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jsharpe.plantlive.UnitTest;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.testing.FixtureHelpers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.io.IOException;
+
+@Category(UnitTest.class)
+public class SummarySerializationTest {
+
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    @Test
+    public void testMinimal() throws IOException {
+        // Given
+        final Summary minimal = new Summary(1, "", 0, 0, 0, 0);
+        final String expected = MAPPER.writeValueAsString(minimal);
+
+        // When
+        final String actual = MAPPER.writeValueAsString(
+                MAPPER.readValue(FixtureHelpers.fixture("fixtures/summary/minimal.json"), Summary.class)
+        );
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNormal() throws IOException {
+        // Given
+        final Summary normal = new Summary(24, "cactus", 24, 73, 60, 4);
+        final String expected = MAPPER.writeValueAsString(normal);
+
+        // When
+        final String actual = MAPPER.writeValueAsString(
+                MAPPER.readValue(FixtureHelpers.fixture("fixtures/summary/normal.json"), Summary.class)
+        );
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+}
